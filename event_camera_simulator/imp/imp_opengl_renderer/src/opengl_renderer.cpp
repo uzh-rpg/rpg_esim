131c793f32a8a4839cfa07899855f565ae0e3ef4
Dario Brescianini <bdario@ethz.ch>
2018-11-02 16:40:50 +0100

    hiding glfw window when rendering with opengö

--- event_camera_simulator/imp/imp_opengl_renderer/src/opengl_renderer.cpp
+++ event_camera_simulator/imp/imp_opengl_renderer/src/opengl_renderer.cpp
@@ -44,6 +44,7 @@ void OpenGLRenderer::init()
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_VISIBLE, GL_FALSE);
 
 #ifdef __APPLE__
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
